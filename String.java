2dc02cdca6700e2e58c09e63bba7c5356b8fe882
KowsikaThangavel <mail2kowsika95@gmail.com>
2016-07-21 15:51:55 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringtoint;
+
+import java.util.Scanner;
+public class Stringtoint {
+
+    
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String a=s.nextLine();
+        int i=Integer.parseInt(a);
+        System.out.println("the integer is:"+a);
+        
+    }
+    
+    }
+  
+
+
+    
+
